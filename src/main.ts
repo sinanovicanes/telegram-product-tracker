99f88bc77887f3ca5d1196ed63c87a439ffb17d7
sinanovicanes <sinanovicanes01@gmail.com>
2024-09-29 17:59:45 +0300

    refactor: Remove commented out code in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,4 @@ const client = TelegramClientFactory.create(process.env.TELEGRAM_BOT_TOKEN);
 
 client.useGlobalGuards(NotBotGuard);
 client.useGlobalGuards(AuthGuard);
-
-// const server = Bun.serve({
-//   port: 8080,
-//   fetch(req: Request): Response | Promise<Response> {
-//     return client.onRequest(req) || new Response("Hello World!");
-//   }
-// });
-
 client.launch().catch(console.error);
-
-// client.initialize(server).catch(console.error);
