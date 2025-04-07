9dcebde018369e6588d0150b1bae7462bba586d8
sinanovicanes <sinanovicanes01@gmail.com>
2025-04-08 02:51:34 +0300

    refactor(zara-scraper): Click button to reveal sizes

--- src/scrapers/zara.scraper.ts
+++ src/scrapers/zara.scraper.ts
@@ -8,6 +8,12 @@ export class ZaraScraper extends Scraper {
 
     const name = await this.getTextContent(".product-detail-info__header-name");
     const price = await this.getTextContent(".money-amount__main");
+
+    // Open size selector
+    await this.page.click(
+      "#main > div > div > div.product-detail-view__main-content > div > div > div.product-detail-size-selector-std.product-detail-info__size-selector > div > div.product-detail-cart-buttons__main-action > button"
+    );
+
     const sizes = await this.page.$$eval(
       ".size-selector-sizes__size:not(.size-selector-sizes__size--disabled) .size-selector-sizes-size__label",
       els => els.map(el => el.textContent)
